7ffe62fa7e933297490fb3bdeb88c7b36632d240
FLORENT CHABERT <florent.chabert@atos.net>
2021-01-15 10:54:15 +0100

    Customize urls (never send/accept pull request for this !)

--- RepoZ.Api.Common/Git/DefaultRepositoryReader.cs
+++ RepoZ.Api.Common/Git/DefaultRepositoryReader.cs
@@ -59,96 +59,108 @@ namespace RepoZ.Api.Common.Git
 
 					var headDetails = GetHeadDetails(repo);
 
-                    return new Api.Git.Repository()
-                    {
-                        Name = workingDirectory.Name,
-                        Path = workingDirectory.FullName,
-                        Location = workingDirectory.Parent.FullName,
-                        Branches = repo.Branches.Select(b => b.FriendlyName).ToArray(),
-                        LocalBranches = repo.Branches.Where(b => !b.IsRemote).Select(b => b.FriendlyName).ToArray(),
-                        AllBranches = this.GetAllBranches(repo),
-                        CurrentBranch = headDetails.Name,
-                        CurrentBranchHasUpstream = !string.IsNullOrEmpty(repo.Head.UpstreamBranchCanonicalName),
-                        CurrentBranchIsDetached = headDetails.IsDetached,
-                        CurrentBranchIsOnTag = headDetails.IsOnTag,
-                        AheadBy = repo.Head.TrackingDetails?.AheadBy,
-                        BehindBy = repo.Head.TrackingDetails?.BehindBy,
-                        LocalUntracked = status?.Untracked.Count(),
-                        LocalModified = status?.Modified.Count(),
-                        LocalMissing = status?.Missing.Count(),
-                        LocalAdded = status?.Added.Count(),
-                        LocalStaged = status?.Staged.Count(),
-                        LocalRemoved = status?.Removed.Count(),
-                        LocalIgnored = status?.Ignored.Count(),
-                        RemoteUrls = this.BuildUrls(repo.Network?.Remotes, headDetails),
+					return new Api.Git.Repository()
+					{
+						Name = workingDirectory.Name,
+						Path = workingDirectory.FullName,
+						Location = workingDirectory.Parent.FullName,
+						Branches = repo.Branches.Select(b => b.FriendlyName).ToArray(),
+						LocalBranches = repo.Branches.Where(b => !b.IsRemote).Select(b => b.FriendlyName).ToArray(),
+						AllBranches = this.GetAllBranches(repo),
+						CurrentBranch = headDetails.Name,
+						CurrentBranchHasUpstream = !string.IsNullOrEmpty(repo.Head.UpstreamBranchCanonicalName),
+						CurrentBranchIsDetached = headDetails.IsDetached,
+						CurrentBranchIsOnTag = headDetails.IsOnTag,
+						AheadBy = repo.Head.TrackingDetails?.AheadBy,
+						BehindBy = repo.Head.TrackingDetails?.BehindBy,
+						LocalUntracked = status?.Untracked.Count(),
+						LocalModified = status?.Modified.Count(),
+						LocalMissing = status?.Missing.Count(),
+						LocalAdded = status?.Added.Count(),
+						LocalStaged = status?.Staged.Count(),
+						LocalRemoved = status?.Removed.Count(),
+						LocalIgnored = status?.Ignored.Count(),
+						RemoteUrls = this.BuildUrls(repo.Network?.Remotes, headDetails),
 						StashCount = repo.Stashes?.Count() ?? 0
-                    };
-                }
-            }
-            catch (Exception)
-            {
-                return Api.Git.Repository.Empty;
-            }
-        }
+					};
+				}
+			}
+			catch (Exception)
+			{
+				return Api.Git.Repository.Empty;
+			}
+		}
 
-        private string[] GetAllBranches(LibGit2Sharp.Repository repo)
-        {
-            var localBranches = repo.Branches.Where(b => !b.IsRemote).Select(b => b.FriendlyName).ToList();
-            // "origin/" is removed from remote branches name and HEAD branch is ignored
-            var strippedRemoteBranches = repo.Branches.Where(b => b.IsRemote && b.FriendlyName.IndexOf("HEAD") == -1).Select(b => b.FriendlyName.Replace("origin/", "")).ToList();
-                        
-            var remoteOnlyBranches = strippedRemoteBranches.Except(localBranches);
-            var localOnlyBranches = localBranches.Except(strippedRemoteBranches);
-            var otherBranches = localBranches.Intersect(strippedRemoteBranches);
-			
+		private string[] GetAllBranches(LibGit2Sharp.Repository repo)
+		{
+			var localBranches = repo.Branches.Where(b => !b.IsRemote).Select(b => b.FriendlyName).ToList();
+			// "origin/" is removed from remote branches name and HEAD branch is ignored
+			var strippedRemoteBranches = repo.Branches.Where(b => b.IsRemote && b.FriendlyName.IndexOf("HEAD") == -1).Select(b => b.FriendlyName.Replace("origin/", "")).ToList();
+
+			var remoteOnlyBranches = strippedRemoteBranches.Except(localBranches);
+			var localOnlyBranches = localBranches.Except(strippedRemoteBranches);
+			var otherBranches = localBranches.Intersect(strippedRemoteBranches);
+
 			// Merge branch lists and sort by name
-            var allBranches = otherBranches
-                              .Union(remoteOnlyBranches.Select(n => string.Concat(n, " (r)")))
-                              .Union(localOnlyBranches.Select(n => string.Concat(n, " (l)")))                
-                              .OrderBy(n => n).ToArray();
+			var allBranches = otherBranches
+							  .Union(remoteOnlyBranches.Select(n => string.Concat(n, " (r)")))
+							  .Union(localOnlyBranches.Select(n => string.Concat(n, " (l)")))
+							  .OrderBy(n => n).ToArray();
 
-            return allBranches;
-        }
+			return allBranches;
+		}
 
-        private string[] BuildUrls(RemoteCollection remotes, HeadDetails headDetails)
-        {
-            List<string> urls = new List<string>();
-            if (remotes != null)
-            {
-                //List<Remote> remotesList = remotes?.Where(r => !string.IsNullOrEmpty(r.Url) && !r.Url.StartsWith("git@")).ToList();
-                foreach (var remote in remotes)
-                {
-                    urls.Add(Urlify(remote.Url, headDetails.Name, "/-/commits/", true));
-                    urls.Add(Urlify(remote.Url, headDetails.Name, "/-/branches/all/", false));
-                }
+		private string[] BuildUrls(RemoteCollection remotes, HeadDetails headDetails)
+		{
+			List<string> urls = new List<string>();
+			if (remotes != null)
+			{
+				//List<Remote> remotesList = remotes?.Where(r => !string.IsNullOrEmpty(r.Url) && !r.Url.StartsWith("git@")).ToList();
+				foreach (var remote in remotes)
+				{
+					if (remote.Url.StartsWith("git@"))
+					{
+						urls.Add(Urlify(remote.Url, headDetails.Name, "/-/commits/", true));
+						urls.Add(Urlify(remote.Url, headDetails.Name, "/-/branches/all/", false));
 
-            }
+					}
+					else
+					{
+						urls.Add(Urlify(remote.Url, headDetails.Name, "/tree/", true));
+					}
+				}
 
-            return urls.ToArray();
-        }
+			}
+
+			return urls.ToArray();
+		}
 
 
 
-        private String Urlify(string gitRemoteUrl, string currentBranch, string path, bool branch)
-        {
-            // Transformer :
-            // git@			git2.april.interne.fr:distribution / aep / tarificateur - aep - web.git
-            // en
-            // https://	    git2.april.interne.fr/distribution/aep/tarificateur-aep-web.git
+		private String Urlify(string gitRemoteUrl, string currentBranch, string path, bool branch)
+		{
+			// Transformer :
+			// git@			git2.april.interne.fr:distribution / aep / tarificateur - aep - web.git
+			// en
+			// https://	    git2.april.interne.fr/distribution/aep/tarificateur-aep-web.git
 
-            string url = gitRemoteUrl;
+			string url = gitRemoteUrl;
 
-            if (gitRemoteUrl.StartsWith("git@"))
-            {
-                url = url.Replace(":", "/");
-                url = url.Replace("git@", "https://");
-                url = url.Replace(".git", "");
-            }
+			if (gitRemoteUrl.StartsWith("git@"))
+			{
+				url = url.Replace(":", "/");
+				url = url.Replace("git@", "https://");
+				url = url.Replace(".git", "");
+			}
+			else
+			{
+				url = url.Replace(".git", "");
+				url = string.Concat(url, path, branch ? currentBranch : "");
+			}
 
-            url = string.Concat(url, path, branch?currentBranch:"");
 
-            return url;
-        }
+			return url;
+		}
 
 		private HeadDetails GetHeadDetails(LibGit2Sharp.Repository repo)
 		{
