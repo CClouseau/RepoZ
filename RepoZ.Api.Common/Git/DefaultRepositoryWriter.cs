f8620623c8622254472f48fc491665be1d8ebbd6
CHABERT, Ext-Florent <ext-florent.chabert@chu-lyon.fr>
2025-10-24 09:30:53 +0200

    Git RPO + upgrade nugets

--- RepoZ.Api.Common/Git/DefaultRepositoryWriter.cs
+++ RepoZ.Api.Common/Git/DefaultRepositoryWriter.cs
@@ -3,23 +3,24 @@ using System.Linq;
 using LibGit2Sharp;
 using RepoZ.Api.Git;
 using RepoZ.Api.Common.Common;
+using System.Xml.Schema;
 
 namespace RepoZ.Api.Common.Git
 {
-	public class DefaultRepositoryWriter : IRepositoryWriter
-	{
-		private readonly IGitCommander _gitCommander;
-		private readonly IAppSettingsService _appSettingsService;
+    public class DefaultRepositoryWriter : IRepositoryWriter
+    {
+        private readonly IGitCommander _gitCommander;
+        private readonly IAppSettingsService _appSettingsService;
 
-		public DefaultRepositoryWriter(IGitCommander gitCommander, IAppSettingsService appSettingsService)
-		{
-			_gitCommander = gitCommander ?? throw new ArgumentNullException(nameof(gitCommander));
-			_appSettingsService = appSettingsService ?? throw new ArgumentNullException(nameof(appSettingsService));
-		}
+        public DefaultRepositoryWriter(IGitCommander gitCommander, IAppSettingsService appSettingsService)
+        {
+            _gitCommander = gitCommander ?? throw new ArgumentNullException(nameof(gitCommander));
+            _appSettingsService = appSettingsService ?? throw new ArgumentNullException(nameof(appSettingsService));
+        }
 
-		public bool Checkout(Api.Git.Repository repository, string branchName)
-		{
-           
+        public bool Checkout(Api.Git.Repository repository, string branchName)
+        {
+
             using (var repo = new LibGit2Sharp.Repository(repository.Path))
             {
                 string realBranchName = branchName;
@@ -34,7 +35,7 @@ namespace RepoZ.Api.Common.Git
                 {
                     // Create local branch to remote branch tip and set its upstream branch to remote
                     var upstreamBranch = repo.Branches.FirstOrDefault(b => b.FriendlyName.EndsWith(branchName));
-                    branch = repo.CreateBranch(branchName, upstreamBranch.Tip);     
+                    branch = repo.CreateBranch(branchName, upstreamBranch.Tip);
                     this.SetUpstream(repository, branchName, upstreamBranch.FriendlyName);
 
                     branch = Commands.Checkout(repo, branchName);
@@ -44,26 +45,31 @@ namespace RepoZ.Api.Common.Git
                 return branch.FriendlyName == branchName;
             }
 
-		}
+        }
 
-		public void Fetch(Api.Git.Repository repository)
-		{
+        public void Fetch(Api.Git.Repository repository)
+        {
             var arguments = _appSettingsService.PruneOnFetch
                 ? new string[] { "fetch", "--all", "--prune" }
                 : new string[] { "fetch", "--all" };
 
-			_gitCommander.Command(repository, arguments);
-		}
 
-		public void Pull(Api.Git.Repository repository)
-		{
-			_gitCommander.Command(repository, "pull");
-		}
+            if (_appSettingsService.PruneOnFetch)
+            {
+                _gitCommander.Command(repository, new string[] { "remote", "prune", "origin" });
+            }
+            _gitCommander.Command(repository, arguments);
+        }
 
-		public void Push(Api.Git.Repository repository)
-		{
-			_gitCommander.Command(repository, "push");
-		}
+        public void Pull(Api.Git.Repository repository)
+        {
+            _gitCommander.Command(repository, "pull");
+        }
+
+        public void Push(Api.Git.Repository repository)
+        {
+            _gitCommander.Command(repository, "push");
+        }
         private void SetUpstream(Api.Git.Repository repository, string localBranchName, string upstreamBranchName)
         {
             _gitCommander.Command(repository, "branch", $"--set-upstream-to={upstreamBranchName}", localBranchName);
